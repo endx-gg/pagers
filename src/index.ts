35e2f7f06ce152ac961c1c3b80391b63246f18e4
Michał Warda <michal.warda@elpassion.pl>
2020-10-30 15:14:13 +0100

    Update name to Handle

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export interface IInputElement {
+export interface IInputHandle {
   getValue(): Promise<any>;
   getError(): Promise<string>;
   hasError(): Promise<boolean>;
@@ -6,28 +6,28 @@ export interface IInputElement {
   setValue(value: any): Promise<void>;
 }
 
-export interface ITextInputElement extends IInputElement {
+export interface ITextInputHandle extends IInputHandle {
   getValue(): Promise<string>;
   getError(): Promise<string>;
   blur(): Promise<void>;
   setValue(value: string): Promise<void>;
 }
 
-export interface INumberInputElement extends IInputElement {
+export interface INumberInputHandle extends IInputHandle {
   getValue(): Promise<number>;
   getError(): Promise<string>;
   blur(): Promise<void>;
   setValue(value: number): Promise<void>;
 }
 
-export interface INumberInputElement extends IInputElement {
+export interface INumberInputHandle extends IInputHandle {
   getValue(): Promise<number>;
   getError(): Promise<string>;
   blur(): Promise<void>;
   setValue(value: number): Promise<void>;
 }
 
-export interface IButtonElement {
+export interface IButtonElementHandle {
   click(): Promise<void>;
   press(): Promise<void>;
   isDisabled(): Promise<boolean>;
