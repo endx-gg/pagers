af65f515b3b5365246a8d58117be684db930298c
Michał Warda <michal.warda@elpassion.pl>
2020-10-30 20:30:32 +0100

    Change order of handle methods

--- src/handles/number.input.handle.ts
+++ src/handles/number.input.handle.ts
@@ -12,16 +12,16 @@ export abstract class NumberInputHandle implements INumberInputHandle {
   getValue(): Promise<number> {
     throw new Error("Method not implemented.");
   }
-  getErrorMessage(): Promise<string | null> {
+  setValue(value: number): Promise<void> {
     throw new Error("Method not implemented.");
   }
   blur(): Promise<void> {
     throw new Error("Method not implemented.");
   }
-  setValue(value: number): Promise<void> {
+  focus(): Promise<void> {
     throw new Error("Method not implemented.");
   }
-  focus(): Promise<void> {
+  getErrorMessage(): Promise<string | null> {
     throw new Error("Method not implemented.");
   }
   hasError(): Promise<boolean> {
